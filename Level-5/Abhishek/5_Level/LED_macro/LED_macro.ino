53c60c0b8f40748f0295b355a2f723fee52d045a
abhishek-bathala <62725573+abhishek-bathala@users.noreply.github.com>
2021-02-08 20:14:13 -0500

    Delete Level-5/Abhishek/5_Level directory

--- Level-5/Abhishek/5_Level/LED_macro/LED_macro.ino
+++
@@ -1,7 +0,0 @@
-#include <avr/io.h>
-#include <util/delay.h>
-#define setbit(x,y)(x|=y)
-#define clearbit(x,y)(x&=~y)
-#define bitn(p)(0x01<<p)
-#define blueon (setbit(PORTB, bit(4)));
-#define blueoff (clearbit(PORTB, bit(4)));
